ceb48bda056a1a26353acce0b401766510a25cdf
Alpar Bartok <bartokalpi@gmail.com>
2022-05-26 01:06:50 +0300

    Add cors support / whitelisting

--- be/main.py
+++ be/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from routers import appointment, user, authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(appointment.router)
 app.include_router(user.router)
